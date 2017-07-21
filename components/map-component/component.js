f09af59f52799cbc6168673c22c728f3285ce4e7
Martin Elsworth <ME@InterSoftNZ.local>
2017-07-21 15:37:14 +1200

    map updates

--- components/map-component/component.js
+++ components/map-component/component.js
@@ -1,7 +1,14 @@
-module.exports = class {
-    onMount() {
-        var position = {lat: -25.363, lng: 131.044};
+module.exports = {
+    onInput() {
+        this.state = {
+            position: {lat: -25.363, lng: 131.044},
+            markers: [],
+            uniqueId: 1,
+            zoom: 8,
+        };
+    },
 
+    onMount() {
         var styledMapType = new google.maps.StyledMapType(
             [
                 {
@@ -22,63 +29,117 @@ module.exports = class {
                     ]
                 }
             ],
-            {name: 'Styled Map'}
+            {name: "Styled Map"}
         )
         
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: position
+        var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: this.state.zoom,
+            center: this.state.position
         });
         
-        //Associate the styled map with the MapTypeId and set it to display.
-        map.mapTypes.set('styled_map', styledMapType);
-        map.setMapTypeId('styled_map');
-
+        // Associate the styled map with the MapTypeId and set it to display
+        map.mapTypes.set("styled_map", styledMapType);
+        map.setMapTypeId("styled_map");
         
-        map.addListener('click', function(e) {
-            addMarker(e);
-        });
+        var markers = this.state.markers;
+        var uniqueId = this.state.uniqueId;
 
-        function addMarker(e) {
+        // Add a Click handler to the Map
+        map.addListener("click", function(e) {
             // Determine the location at the click position
             var location = e.latLng;
- 
+
             // Create a marker and place it on the map
             var marker = new google.maps.Marker({
                 position: location,
                 map: map
             });
- 
-            // AssignMarker a unique id
+
+            // Assign the Marker a unique id
             marker.id = uniqueId;
             uniqueId++;
- 
+
             // Attach click event handler to the marker
             google.maps.event.addListener(marker, "click", function (e) {
-                var content = 'Latitude: ' + location.lat() + '<br />Longitude: ' + location.lng();
-                content += "<br /><input type = 'button' value = 'Delete' onclick = 'DeleteMarker(" + marker.id + ");' value = 'Delete' />";
+                /*var content = "Latitude: " + location.lat() + "<br />Longitude: " + location.lng();
+                content += "<br /><input type = 'button' value = 'Delete' onclick = 'deleteMarker(" + marker.id + ");' value = 'Delete' />";
                 var infoWindow = new google.maps.InfoWindow({
                     content: content
                 });
-                infoWindow.open(map, marker);
+                infoWindow.open(map, marker);*/
+                
+                // Find and remove the marker from the Array
+                for (var i = 0; i < markers.length; i++) {
+                    if (markers[i].id == marker.id) {
+                        // Remove the marker from Map                  
+                        markers[i].setMap(null);
+
+                        // Remove the marker from array
+                        markers.splice(i, 1);
+                        return;
+                    }
+                }
             });
- 
+
             // Add marker to the array
             markers.push(marker);
-        }
+        });
 
-        function DeleteMarker(id) {
+        function deleteMarker(id) {
             // Find and remove the marker from the Array
             for (var i = 0; i < markers.length; i++) {
                 if (markers[i].id == id) {
                     // Remove the marker from Map                  
                     markers[i].setMap(null);
-    
+
                     // Remove the marker from array
                     markers.splice(i, 1);
                     return;
                 }
             }
         }
+    },
+
+    addMarker(e, map) {
+        // Determine the location at the click position
+        var location = e.latLng;
+
+        // Create a marker and place it on the map
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+
+        // Assign the Marker a unique id
+        marker.id = uniqueId;
+        uniqueId++;
+
+        // Attach click event handler to the marker
+        google.maps.event.addListener(marker, "click", function (e) {
+            var content = "Latitude: " + location.lat() + "<br />Longitude: " + location.lng();
+            content += "<br /><input type = 'button' value = 'Delete' onclick = 'deleteMarker(" + marker.id + ");' value = 'Delete' />";
+            var infoWindow = new google.maps.InfoWindow({
+                content: content
+            });
+            infoWindow.open(map, marker);
+        });
+
+        // Add marker to the array
+        markers.push(marker);
     }
-}
+};
+
+/*  
+function deleteMarker(id) {
+    // Find and remove the marker from the Array
+    for (var i = 0; i < markers.length; i++) {
+        if (markers[i].id == id) {
+            // Remove the marker from Map                  
+            markers[i].setMap(null);
+
+            // Remove the marker from array
+            markers.splice(i, 1);
+            return;
+        }
+    }
+}*/
