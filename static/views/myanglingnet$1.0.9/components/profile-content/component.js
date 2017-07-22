96ce08289c7992dffd249375d05bb25a8f98acfe
Mart <myanglingnet@gmail.com>
2017-07-22 21:53:21 +1200

    profile component added

---
+++ static/views/myanglingnet$1.0.9/components/profile-content/component.js
@@ -0,0 +1,8 @@
+$_mod.def("/myanglingnet$1.0.9/components/profile-content/component", function(require, exports, module, __filename, __dirname) { /*module.exports = {
+    onInput(input) {
+        this.state = {
+            user: input.user
+        }
+    }
+}*/
+});
