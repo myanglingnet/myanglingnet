a398974cc6c1c12e4aa4ec75796d07b66ec6374d
Mart <myanglingnet@gmail.com>
2017-08-14 16:19:45 +1200

    Moved marker logic to modal component

--- components/modal-window/component.js
+++ components/modal-window/component.js
@@ -1,28 +1,73 @@
 module.exports = {
     onInput() {
         this.state = {
-            map: null
+            map: null,
+            latlngPosition: null
         }
     },
-    onMount() {
-        
-    },
-    showModal() {
+    
+    // Show the modal       
+    showModal(latlngPosition) { 
         var modal = this.getEl("modal-window");
         modal.style.display = "block";
+
+        // Set the lat/lng of the map marker
+        this.state.latlngPosition = latlngPosition;
     },
-    closeModal() {
+
+    // Hide the modal
+    hideModal() {
         var modal = this.getEl("modal-window");
         modal.style.display = "none";
     },
+    
+    // Update modal text
     updateModalText(text) {
         var modalText = this.getEl("modal-text");
         modalText.innerHTML = text;
     },
+    
+    // Link the map to the modal
     setMap(map) {
         this.state.map = map;
     },
-    addMapItem() {
-        this.emit("update");
+    
+    // Add a marker to the map
+    addMapMarker() {
+        // Hide the modal
+        this.hideModal();
+
+        // Marker icon
+        var markerIcon = {
+            url: "../../static/catch-pin.png",
+            scaledSize: new google.maps.Size(40, 52),
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(20, 50)
+        };
+
+        // Create a marker and place it on the map
+        var marker = new google.maps.Marker({
+            position: this.state.latlngPosition,
+            animation: google.maps.Animation.DROP,
+            icon:markerIcon,
+            map: this.state.map
+        });
+        
+        // Add a click listener to the marker
+        google.maps.event.addListener(marker, "click", function (e) {
+            var lat = e.latLng.lat();
+            var lng = e.latLng.lng();
+
+            // Restrict lat/lng to 4dp precision
+            lat = lat.toFixed(4);
+            lng = lng.toFixed(4);
+
+            // Show the modal
+            var modal = document.getElementsByClassName("modal")[0];
+            modal.style.display = "block";
+
+            var modalText = document.getElementsByClassName("modal-text")[0];
+            modalText.innerHTML = "lat: " + lat + "; lng: " + lng;
+        });
     }
 }
--- static/views/myanglingnet$1.0.9/components/map-component/component.js
+++ static/views/myanglingnet$1.0.9/components/map-component/component.js
@@ -11,9 +11,6 @@ module.exports = {
     },
 
     onMount() {
-        // Get the modal component
-        var modal = this.getComponent("modal");
-
         // Map settings
         var styledMapType = new google.maps.StyledMapType(
             [
@@ -39,17 +36,20 @@ module.exports = {
         )
 
         // Create the map
-        //var map = new google.maps.Map(document.getElementById("map"), {
         map = new google.maps.Map(document.getElementById("map"), {
             zoom: this.state.zoom,
             center: this.state.position
         });
-        
-        modal.setMap(map);
-
+    
         // Associate the styled map with the MapTypeId and set it to display
         map.mapTypes.set("styled_map", styledMapType);
         map.setMapTypeId("styled_map");
+
+        // Get the modal component
+        var modal = this.getComponent("modal");
+
+        // Set the map object on the modal
+        modal.setMap(map);
 
         // Attach a click listener to the Map to record new markers
         map.addListener("click", function(e) {
@@ -64,15 +64,14 @@ module.exports = {
             lat = lat.toFixed(4);
             lng = lng.toFixed(4);
             
-            // Show the modal
-            modal.showModal();
-            modal.updateModalText("Latitude: " + lat + "  Longitude: " + lng);
-
             // Create lat/lng object
             latlngPosition = {lat: + lat, lng: + lng};
 
+            // Show the modal
+            modal.showModal(latlngPosition);
+            
             // Add a marker at the position
-            AddMarker(latlngPosition, map);            
+            //AddMarker(latlngPosition, map);            
             
             // Create post data for the request
             var postData = { lat, lng, mapEntryId }
@@ -82,12 +81,7 @@ module.exports = {
             xhr.open("POST", "/mapentry");
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.onload = function() {
-                //if (xhr.status === 200) {
-                //    alert(xhr.responseText);
-                //}
-                //else {
-                //    alert("Request failed and returned a status of " + xhr.status);
-                //}
+                // Handle response - success/failure
             };
             xhr.send(JSON.stringify(postData));                        
         })
@@ -159,120 +153,6 @@ module.exports = {
                 modal.updateModalText("Latitude: " + lat + "  Longitude: " + lng);
             });
         }
-
-        // Add a Click handler to the Map
-        /*map.addListener("click", function(e) {
-            // Determine the location at the click position
-            var location = e.latLng;
-            
-            // Marker icon
-            var markerIcon = {
-                url: "../../static/catch-pin.png",
-                scaledSize: new google.maps.Size(32, 45),
-                origin: new google.maps.Point(0,0),
-                anchor: new google.maps.Point(15, 45)
-            };
-
-            // Create a marker and place it on the map
-            var marker = new google.maps.Marker({
-                position: location,
-                animation: google.maps.Animation.DROP,
-                icon:markerIcon,
-                map: map
-            });
-
-            // Assign the Marker a unique id
-            marker.id = mapEntryId;
-            mapEntryId++;
-
-            // Display current coordinates on hover
-            google.maps.event.addListener(marker, "mouseover", function (e) {
-                displayCoordinates(e.latLng, map, marker);
-            });
-            
-            // Hide the infowindow when user leaves hover
-            marker.addListener("mouseout", function() {
-                infowindow.close();
-            });
-
-            // Attach click event handler to the marker
-            google.maps.event.addListener(marker, "click", function (e) {    
-                // Find and remove the marker from the Array
-                for (var i = 0; i < markers.length; i++) {
-                    if (markers[i].id == marker.id) {
-                        // Remove the marker from Map                  
-                        markers[i].setMap(null);
-
-                        // Remove the marker from array
-                        markers.splice(i, 1);
-                        return;
-                    }
-                }
-            });
-
-            // Add marker to the array
-            markers.push(marker);
-
-            // Get the current lat/lng
-            var lat = e.latLng.lat();
-            var lng = e.latLng.lng();
-
-            // Restrict lat/lng values to 6dp
-            lat = lat.toFixed(6);
-            lng = lng.toFixed(6);
-
-            var postData = { lat, lng, mapEntryId }
-
-            // Create a new POST request
-            xhr = new XMLHttpRequest();
-            xhr.open("POST", "/mapentry");
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.onload = function() {
-                console.log(xhr.status + ": " + xhr.responseText);
-
-                //if (xhr.status === 200) {
-                //    alert(xhr.responseText);
-                //}
-                //else {
-                //    alert("Request failed and returned a status of " + xhr.status);
-                //}
-            };
-            xhr.send(JSON.stringify(postData));
-        });*/
-
-        /*function deleteMarker(id) {
-            // Find and remove the marker from the Array
-            for (var i = 0; i < markers.length; i++) {
-                if (markers[i].id == id) {
-                    // Remove the marker from Map                  
-                    markers[i].setMap(null);
-
-                    // Remove the marker from array
-                    markers.splice(i, 1);
-                    return;
-                }
-            }
-        }
- 
-        function displayCoordinates(coordinates, map, marker) {
-            var lat = coordinates.lat();
-            var lng = coordinates.lng();
-
-            lat = lat.toFixed(4);
-            lng = lng.toFixed(4);
-
-            // Create an info window object
-            infowindow = new google.maps.InfoWindow({
-                content: "Latitude: " + lat + "  Longitude: " + lng
-            });
-
-            // Display the info window 
-            infowindow.open(map, marker);
-        }*/
     },
-
-    loadMarkers() {
-
-    }
 };
 });
